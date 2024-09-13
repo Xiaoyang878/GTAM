c3d1159da5625ecf85cf103620346bd828648849
Tian Zhu <zhutian22s@ict.ac.cn>
2024-09-13 11:30:56 +0800

    Update GTA.py

--- modules/models/GTA.py
+++ modules/models/GTA.py
@@ -19,27 +19,8 @@ import math
 
 logger = logging.getLogger(__name__)
 
-def get_timestep_embedding(timesteps, embedding_dim, max_positions=10000):
-    # Code from https://github.com/hojonathanho/diffusion/blob/master/diffusion_tf/nn.py
-    """
-    From Fairseq.Build sinusoidal embeddings.This matches the implementation in tensor2tensor, but differs slightly
-    from the description in Section 3.5 of "Attention Is All You Need".
-    """
-    assert len(timesteps.shape) == 1
-    timesteps = timesteps * max_positions
-    half_dim = embedding_dim // 2
-    emb = math.log(max_positions) / (half_dim - 1)
-    emb = torch.exp(torch.arange(half_dim, dtype=torch.float32, device=timesteps.device) * -emb)
-    emb = timesteps.float()[:, None] * emb[None, :]
-    emb = torch.cat([torch.sin(emb), torch.cos(emb)], dim=1)
-    if embedding_dim % 2 == 1: # Zero pad
-        emb = F.pad(emb, (0, 1), mode='constant')
-    assert emb.shape == (timesteps.shape[0], embedding_dim)
-    return emb  
-
-
 class Attention(nn.Module):
-    def __init__(self, input_dim, key_dim, value_dim, output_dim, num_head,split_first=True, gating=True,inp_kernels=None, config=None):
+    def __init__(self, input_dim, key_dim, value_dim, output_dim, num_head, gating=True,config=None):
         super().__init__()
         assert key_dim % num_head == 0
         assert value_dim % num_head == 0
@@ -48,27 +29,18 @@ class Attention(nn.Module):
 
         self.num_head = num_head
         
-        self.split_first = split_first
 
-        if self.split_first:
-            self.proj_q = Linear(input_dim, key_dim, init='attn', bias=False, config=config)
-            self.proj_k = Linear(input_dim, key_dim, init='attn', bias=False, config=config)
-            self.proj_v = Linear(input_dim, value_dim, init='attn', bias=False, config=config)
-        else:
-            assert (key_dim == value_dim)
-            self.proj_in = Linear(input_dim, key_dim * 3, init='attn', bias=False, config=config)
-        
+        self.proj_q = Linear(input_dim, key_dim, init='attn', bias=False, config=config)
+        self.proj_k = Linear(input_dim, key_dim, init='attn', bias=False, config=config)
+        self.proj_v = Linear(input_dim, value_dim, init='attn', bias=False, config=config)
+       
         self.gating = gating
         if gating:
             self.gate= Linear(input_dim, value_dim, init='gate', config=config)
 
         self.proj_out = Linear(value_dim, output_dim, init='final', config=config)
          
-        self.inp_kernels = inp_kernels
-        if inp_kernels:
-            self.inp_q = SpatialDepthWiseInception(key_dim // num_head, inp_kernels)
-            self.inp_k = SpatialDepthWiseInception(key_dim // num_head, inp_kernels)
-            self.inp_v = SpatialDepthWiseInception(value_dim // num_head, inp_kernels)
+
 
     def forward(self, q_data, k_data=None, bias=None, k_mask=None):
         """
@@ -82,23 +54,13 @@ class Attention(nn.Module):
         """
         key_dim, value_dim = self.key_dim // self.num_head, self.value_dim // self.num_head
         
-        if self.split_first:
-            assert (k_data is not None)
-            q = self.proj_q(q_data) 
-            k = self.proj_k(k_data)
-            v = self.proj_v(k_data)
-            q, k, v = map(lambda t: rearrange(t, 'b s l (h d) -> b s h l d', h = self.num_head), (q, k, v))
-        else:
-            assert (k_data is None)
-            t = rearrange(self.proj_in(q_data), "... l (h d) -> ... h l d", h=self.num_head)
-            q, k, v = torch.chunk(t, 3, dim=-1)
         
-        if self.inp_kernels:
-            q, k, v = map(lambda t: rearrange(t, 'b s h l d-> b (s h) l d'), (q, k, v))
-            q = self.inp_q(q)
-            k = self.inp_k(k)
-            v = self.inp_v(v)
-            q, k, v = map(lambda t: rearrange(t, 'b (s h) l d-> b s h l d', h = self.num_head), (q, k, v))
+        assert (k_data is not None)
+        q = self.proj_q(q_data) 
+        k = self.proj_k(k_data)
+        v = self.proj_v(k_data)
+        q, k, v = map(lambda t: rearrange(t, 'b s l (h d) -> b s h l d', h = self.num_head), (q, k, v))
+        
         
         q = q* key_dim**(-0.5)
 
@@ -129,13 +91,10 @@ class SeqAttentionWithPairBias(nn.Module):
     def __init__(self, config, num_in_seq_channel, num_in_pair_channel):
         super().__init__()
         c = config
-        try:
-            LoRA_conf = c.LoRA
-        except:
-            LoRA_conf = None
+       
         self.seq_norm = LayerNorm(num_in_seq_channel)
         self.pair_norm = LayerNorm(num_in_pair_channel)
-        self.proj_pair = Linear(num_in_pair_channel, c.num_head, init='linear', bias = False, config=LoRA_conf)
+        self.proj_pair = Linear(num_in_pair_channel, c.num_head, init='linear', bias = False, config=None)
 
         self.attn = Attention(
                 input_dim=num_in_seq_channel,
@@ -143,9 +102,7 @@ class SeqAttentionWithPairBias(nn.Module):
                 value_dim=num_in_seq_channel,
                 output_dim=num_in_seq_channel,
                 num_head=c.num_head,
-                split_first=False,
-                inp_kernels=c.inp_kernels,
-                config=LoRA_conf)
+                config=None)
 
         self.config = config
 
@@ -175,10 +132,7 @@ class Transition(nn.Module):
         super().__init__()
 
         c = config
-        try:
-            LoRA_conf = c.LoRA
-        except:
-            LoRA_conf = None
+        
 
         if num_out_channel is None:
             num_out_channel = num_in_channel
@@ -186,30 +140,26 @@ class Transition(nn.Module):
         intermediate_channel = num_in_channel * c.num_intermediate_factor
         self.transition = nn.Sequential(
                 LayerNorm(num_in_channel),
-                Linear(num_in_channel, intermediate_channel, init='linear', config=LoRA_conf),
+                Linear(num_in_channel, intermediate_channel, init='linear', config=None),
                 nn.ReLU(),
-                Linear(intermediate_channel, num_out_channel, init='final', config=LoRA_conf),
+                Linear(intermediate_channel, num_out_channel, init='final', config=None),
                 )
 
     def forward(self, act, mask):
         return self.transition(act)
 
-# AF2 and ESM-FOLD have different implementations
-# Here we just follow ESMFOLD
+
 class OuterProductMean(nn.Module):
     def __init__(self, config, num_in_channel, num_out_channel):
         super().__init__()
 
         c = config
-        try:
-            LoRA_conf = c.LoRA
-        except:
-            LoRA_conf = None
+       
         self.norm = LayerNorm(num_in_channel)
-        self.left_proj = Linear(num_in_channel, c.num_outer_channel, init='linear', config=LoRA_conf)
-        self.right_proj = Linear(num_in_channel, c.num_outer_channel, init='linear', config=LoRA_conf)
+        self.left_proj = Linear(num_in_channel, c.num_outer_channel, init='linear', config=None)
+        self.right_proj = Linear(num_in_channel, c.num_outer_channel, init='linear', config=None)
 
-        self.out_proj = Linear(2 * c.num_outer_channel, num_out_channel, init='final', config=LoRA_conf)
+        self.out_proj = Linear(2 * c.num_outer_channel, num_out_channel, init='final', config=None)
 
     def forward(self, act, mask):
         """
@@ -239,28 +189,20 @@ class TriangleMultiplication(nn.Module): # pair_mask
         super().__init__()
         c = config
         assert c.orientation in ['per_row', 'per_column']
-        try:
-            LoRA_conf = c.LoRA
-        except:
-            LoRA_conf = None
+        
         self.norm = LayerNorm(num_in_channel)
 
-        self.left_proj = Linear(num_in_channel, c.num_intermediate_channel, init='linear', config=LoRA_conf)
-        self.right_proj = Linear(num_in_channel, c.num_intermediate_channel, init='linear', config=LoRA_conf)
+        self.left_proj = Linear(num_in_channel, c.num_intermediate_channel, init='linear', config=None)
+        self.right_proj = Linear(num_in_channel, c.num_intermediate_channel, init='linear', config=None)
 
         self.final_norm = LayerNorm(c.num_intermediate_channel)
         
         if c.gating:
-            self.left_gate = Linear(num_in_channel, c.num_intermediate_channel, init='gate', config=LoRA_conf)
-            self.right_gate = Linear(num_in_channel, c.num_intermediate_channel, init='gate', config=LoRA_conf)
-            self.final_gate = Linear(num_in_channel, num_in_channel, init='gate', config=LoRA_conf)
+            self.left_gate = Linear(num_in_channel, c.num_intermediate_channel, init='gate', config=None)
+            self.right_gate = Linear(num_in_channel, c.num_intermediate_channel, init='gate', config=None)
+            self.final_gate = Linear(num_in_channel, num_in_channel, init='gate', config=None)
         
-        self.proj_out = Linear(c.num_intermediate_channel, num_in_channel, init='final', config=LoRA_conf)
-
-        
-        if c.inp_kernels:
-            self.inp_left = SpatialDepthWiseInception(c.num_intermediate_channel // c.num_head, c.inp_kernels)
-            self.inp_right = SpatialDepthWiseInception(c.num_intermediate_channel // c.num_head, c.inp_kernels)
+        self.proj_out = Linear(c.num_intermediate_channel, num_in_channel, init='final', config=None)
 
         self.config = c
 
@@ -282,25 +224,6 @@ class TriangleMultiplication(nn.Module): # pair_mask
         left_proj_act = self.left_proj(act)
         right_proj_act = self.right_proj(act)
         
-        if c.inp_kernels:
-            if c.orientation == 'per_row':
-                equation = 'b i j (h d) -> b (i h) j d'
-            else:
-                equation = 'b i j (h d) -> b (j h) i d'
-
-            left_proj_act, right_proj_act = map(
-                    lambda t: rearrange(t, equation, h = c.num_head), (left_proj_act, right_proj_act))
-
-            left_proj_act = self.inp_left(left_proj_act)
-            right_proj_act = self.inp_right(right_proj_act)
-            
-            if c.orientation == 'per_row':
-                equation = 'b (i h) j d -> b i j (h d)'
-            else:
-                equation = 'b (j h) i d -> b i j (h d)'
-            
-            left_proj_act, right_proj_act = map(
-                    lambda t: rearrange(t, equation, h = c.num_head), (left_proj_act, right_proj_act))
         
         left_proj_act = pair_mask * left_proj_act
         right_proj_act = pair_mask * right_proj_act
@@ -335,13 +258,10 @@ class TriangleAttention(nn.Module): # seq_mask
         c = config
 
         assert c.orientation in ['per_row', 'per_column']
-        try:
-            LoRA_conf = c.LoRA
-        except:
-            LoRA_conf = None
+        
 
         self.norm = LayerNorm(num_in_pair_channel)
-        self.proj_pair = Linear(num_in_pair_channel, c.num_head, init='linear', bias = False, config=LoRA_conf)
+        self.proj_pair = Linear(num_in_pair_channel, c.num_head, init='linear', bias = False, config=None)
         self.attn = Attention(
                 input_dim=num_in_pair_channel,
                 key_dim=num_in_pair_channel,
@@ -349,8 +269,7 @@ class TriangleAttention(nn.Module): # seq_mask
                 output_dim=num_in_pair_channel,
                 num_head=c.num_head,
                 gating=c.gating,
-                inp_kernels=c.inp_kernels,
-                config=LoRA_conf)
+                config=None)
 
         self.config = config
 
@@ -451,60 +370,9 @@ class gtaformer(nn.Module):
             block_fn = functools.partial(block, seq_mask=seq_mask, pair_mask=pair_mask)
             if self.training and not is_recycling and it > 0:
                 seq_act, pair_act = checkpoint(block_fn, seq_act, pair_act)
-                # for name, param in block.named_parameters():
-                #     if param.requires_grad:
-                #         print(f"Parameter name: {name}")
-                #         print("Parameter value: ", param.data)
-                #         print("Parameter gradient: ", param.grad)
-                # print(f"seq_act: {seq_act}")
-                # print(f"pair_act: {pair_act}")
-                #seq_act, pair_act = block_fn(seq_act, pair_act)
+                
             else:
                 seq_act, pair_act = block_fn(seq_act, pair_act)
         return seq_act, pair_act
 
 
-class SpatialDepthWiseConvolution(nn.Module):
-    def __init__(self, head_dim: int, kernel_size: int = 3):
-        super().__init__()
-        self.kernel_size = kernel_size
-        self.conv = nn.Conv1d(in_channels=head_dim, out_channels=head_dim,
-                kernel_size=(kernel_size,),
-                # padding=(kernel_size - 1,),
-                padding=kernel_size//2,
-                groups=head_dim)
-    
-    def forward(self, x: torch.Tensor):
-        batch_size, heads, seq_len, head_dim = x.shape
-        x = x.permute(0, 1, 3, 2).contiguous()
-        x = x.view(batch_size * heads, head_dim, seq_len)
-        x = self.conv(x)
-        #if self.kernel_size>1:
-        #    x = x[:, :, :-(self.kernel_size - 1)]
-        x = x.view(batch_size, heads, head_dim, seq_len)
-        x = x.permute(0, 1, 3, 2)
-        return x
-
-
-class SpatialDepthWiseInception(nn.Module):
-    def __init__(self, head_dim, kernels):
-        super().__init__()
-       
-        assert len(kernels) > 1 and  kernels[0] == 1
-
-        self.convs = torch.nn.ModuleList([SpatialDepthWiseConvolution(head_dim, kernel_size=k) for k in kernels[1:]])
-        self.kernels = kernels
-    def forward(self, x):
-        # x: (batch, num_heads, len, head_dim)
-        
-        assert x.shape[1] % len(self.kernels) == 0
-        group_num_head = x.shape[1] // len(self.kernels)
-        
-        outputs = [x[:,:group_num_head]]
-
-        for i, conv in enumerate(self.convs):
-            outputs.append(conv(x[:,group_num_head*(i+1):group_num_head*(i+2)]))
-
-        outputs = torch.cat(outputs, dim=1)
-
-        return outputs
